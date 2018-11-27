c26bf45c204801fb9b95ccd128640ee807b20d57
Venky Venkataraman <venky76v@gmail.com>
2018-11-27 19:20:14 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { ExampleNg6LibModule } from './../../projects/example-ng6-lib/src/lib/example-ng6-lib.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ExampleNg6LibComponent } from 'example-ng6-lib';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ExampleNg6LibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
